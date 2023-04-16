630441288e438736d0ac3279bb15e937cfc7be24
dladncks1217 <dlaxodud1217@gmail.com>
2023-04-16 11:33:11 +0900

    refactor: filterByCategory, sortBy as 타입 제거

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Select from "./Select";
-import type { Category, SortBy } from "../types/Restaurant";
 import RestaurantList from "./RestaurantList";
 import { filterByCategory, getRestaurants, sortBy } from "../utils/restaurant";
 
@@ -12,8 +11,8 @@ const Main = () => {
   const [category, handleCategory] = useSelect("전체");
   const [sortByData, handleSort] = useSelect("이름순");
 
-  const filteredData = filterByCategory(restaurants, category as Category);
-  const sortedData = sortBy(filteredData, sortByData as SortBy);
+  const filteredData = filterByCategory(restaurants, category);
+  const sortedData = sortBy(filteredData, sortByData);
 
   return (
     <>
